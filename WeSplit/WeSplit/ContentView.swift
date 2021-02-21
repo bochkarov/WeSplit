2dd7efb4c3213d4c0c44d7d8aacac15597613a5f
Valentyn Bochkarov <bochkarov90@gmail.com>
2021-02-21 19:51:56 +0100

    - Add a header to the third section, saying “Amount per person” - Add another section showing the total amount for the check – i.e., the original amount plus tip value, without dividing by the number of people. - Change the “Number of people” picker to be a text field, making sure to use the correct keyboard type.

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -9,9 +9,24 @@ import SwiftUI
 
 struct ContentView: View {
     @State var checkAmount = ""
-    @State var numberOfPeople = 2
+    @State var numberOfPeople = ""
     @State var tipPercentage = 2
     let tipPercentages = [10, 15, 20, 25, 0]
+   
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople) ?? 1
+        let tipSelection = Double(tipPercentages[tipPercentage])
+        let orderAmount = Double(checkAmount) ?? 0
+        let tipValue = orderAmount / 100 * tipSelection
+        let grandTotal = orderAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+
+        return amountPerPerson
+    }
+    var totalAmount: Double {
+        let peopleCount = Double(numberOfPeople) ?? 1
+        return totalPerPerson * peopleCount
+    }
     
     var body: some View {
         NavigationView {
@@ -20,20 +35,30 @@ struct ContentView: View {
                 Section {
                     TextField("Amount", text: $checkAmount)
                         .keyboardType(.decimalPad)
-                    Picker("Number of people", selection: $numberOfPeople) {
-                        ForEach(2 ..< 100) {
-                            Text("\($0) people")
-                        }
-                    }
+//                    Picker("Number of people", selection: $numberOfPeople) {
+//                        ForEach(2 ..< 100) {
+//                            Text("\($0) people")
+//                        }
+//                    }
+                    TextField("Number of people", text: $numberOfPeople)
+                        .keyboardType(.numberPad)
+                
                 }
                 Section(header: Text("How much tip do you want to leave?")) {
-                   
+
                     Picker("Tip percentage", selection: $tipPercentage) {
                         ForEach(0 ..< tipPercentages.count) {
                             Text(" \(self.tipPercentages[$0])%")
                         }
                     }
                     .pickerStyle(SegmentedPickerStyle())
+                }
+                
+                Section(header: Text("Amount per person")){
+                    Text("$\(totalPerPerson, specifier: "%.2f")")
+                }
+                Section(header: Text("Total amount")){
+                    Text("$\(totalAmount, specifier: "%.2f")")
                 }
             }
             .navigationBarTitle("WeSplit")
